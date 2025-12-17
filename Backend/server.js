ffb98f4b0e2974ecde65dfc7466bd9d7239efa53
Tilak <adireddytilak@gmail.com>
2025-12-17 15:44:18 +0530

    added middleware for headers

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,19 @@ const allowedOrigins = [
   "http://localhost:3000"
 ];
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://pure-basket-zeta.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use(cors({
   origin: allowedOrigins,
   methods: "GET,POST,PUT,DELETE",
